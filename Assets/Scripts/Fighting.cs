4740f388bace35897810b6895417a84f3edbe2de
thansson2004 <79506289+thansson2004@users.noreply.github.com>
2022-01-18 13:51:11 +0100

    Ehm

---
+++ Assets/Scripts/Fighting.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighting : MonoBehaviour
+{
+    public int damage;
+    public int maxHP;
+    public int currentHP;
+    public string unitName;
+}
