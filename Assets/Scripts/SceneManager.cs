d7fffafc4fd66f010fe0276feb5cd86acb08ee00
BiggieCheese3 <Morgan.rockner@elev.ga.lbs.se>
2022-01-18 12:52:49 +0100

    Update SceneManager.cs

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,5 +14,10 @@ public class SceneManager : MonoBehaviour
             // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
             SceneManager.LoadScene("OtherSceneName", LoadSceneMode.Additive);
         }
+    }
+
+    private static void LoadScene(string v, LoadSceneMode additive)
+    {
+        throw new NotImplementedException();
     }
 }
