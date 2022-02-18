aa005ebc0ec0fefefa982d9526046e81751adff4
Astromech-ai <79506266+Astromech-ai@users.noreply.github.com>
2022-02-18 14:01:12 +0100

    Kommenterad kod

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,24 +1,25 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// Ian
 public class Movement : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float speed = .01f;
+    public float speed = .01f;  
 
     
 
     // Update is called once per frame
     void Update()
     {
-        float xDirection = Input.GetAxis("Horizontal");
-        float yDirection = Input.GetAxis("Vertical");
+        float xDirection = Input.GetAxis("Horizontal"); //Gör så att man kan röra sig upp och ner
+        float yDirection = Input.GetAxis("Vertical"); // Gör så att man kan röra sig höger och vänster
 
-        Vector3 moveDirection = new Vector3(xDirection, yDirection, 0.0f);
+        Vector3 moveDirection = new Vector3(xDirection, yDirection, 0.0f); // Gör så att man kan röra sig
 
-        transform.position += moveDirection * speed;
+        transform.position += moveDirection * speed; // Räknar ut om hur snabbt man går åt de hållet man går
 
+        // ᏇᎩፈᎤᏖᏂåᏒ
         animator.SetFloat("Horizontal", xDirection);
         animator.SetFloat("Vertical", yDirection);
         
